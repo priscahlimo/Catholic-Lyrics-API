31cb5aa78ca010fcb25f5bb6319488dec9a79921
priscahlimo <priscahlimoh@gmail.com>
2022-04-04 18:15:22 +0300

    update db credentials

--- src/main/java/dao/DB.java
+++ src/main/java/dao/DB.java
@@ -4,6 +4,6 @@ import org.sql2o.Sql2o;
 
 public class DB {
 
-        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/catholic", "moringa", "briellepriscah");
+        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-73-155-171.compute-1.amazonaws.com", "dkecmhiasjboxr", "ba42acde677a488b468cd5b1cddbbbfdb5b2171051d460bf7a87cc75d28e5a87");
 
 }
