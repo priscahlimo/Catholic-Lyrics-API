16391826c6a37882b1f89508507d7b76cad49f85
priscahlimo <priscahlimoh@gmail.com>
2022-04-05 11:25:35 +0300

    change postgress credentials

--- src/main/java/dao/DB.java
+++ src/main/java/dao/DB.java
@@ -4,6 +4,6 @@ import org.sql2o.Sql2o;
 
 public class DB {
 
-        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-73-155-171.compute-1.amazonaws.com", "adflnffstgxosc", "dea8c6b82f62fc967f9cdadb1cc74a7a877398bf489ff4f736d194f77db8bbe5");
+        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-3-230-122-20.compute-1.amazonaws.com", "wuzfaxmdgxbuwo", "0f64121e86ab0ad43b7abfddbfb88a462e102efb2901aa2791354a99c5a86966");
 
 }
