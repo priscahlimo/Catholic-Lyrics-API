3fc477db2f3082d0e82ad3132e5134f06482c0eb
priscahlimo <priscahlimoh@gmail.com>
2022-04-05 11:16:39 +0300

    change verion of postgress in pom.xml

--- src/main/java/dao/DB.java
+++ src/main/java/dao/DB.java
@@ -4,6 +4,6 @@ import org.sql2o.Sql2o;
 
 public class DB {
 
-        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-73-155-171.compute-1.amazonaws.com", "dkecmhiasjboxr", "ba42acde677a488b468cd5b1cddbbbfdb5b2171051d460bf7a87cc75d28e5a87");
+        public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-73-155-171.compute-1.amazonaws.com", "adflnffstgxosc", "dea8c6b82f62fc967f9cdadb1cc74a7a877398bf489ff4f736d194f77db8bbe5");
 
 }
